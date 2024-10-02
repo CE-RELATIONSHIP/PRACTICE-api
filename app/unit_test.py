b93ca4a6fb8f3cf7f9af4761e22ddb7407ee3281
Jirapat Treesuwan <jirapat3774@gmail.com>
2024-10-02 09:18:51 +0700

    feat: add next5

--- app/unit_test.py
+++ app/unit_test.py
@@ -5,20 +5,20 @@ import app
 from app import app as flask_instance
 
 class AppTestCase(unittest.TestCase):
-    def test_when_x_is_17(self):
-        res = app.is_prime(17).response.pop()
-        res = res.decode("utf-8").strip() == 'true'
-        self.assertEqual(res, True)
+    def test_when_x_is_1(self):
+        res = app.next5(1).response.pop()
+        res = float(res)
+        self.assertEqual(res, 6)
     
-    def test_when_x_is_36(self):
-        res = app.is_prime(36).response.pop()
-        res = res.decode("utf-8").strip() == 'true'
-        self.assertEqual(res, False)
+    def test_when_x_is_neg10(self):
+        res = app.next5(-10).response.pop()
+        res = float(res)
+        self.assertEqual(res, -5)
     
-    def test_when_x_is_13219(self):
-        res = app.is_prime(13219).response.pop()
-        res = res.decode("utf-8").strip() == 'true'
-        self.assertEqual(res, True)
+    def test_when_x_is_1dot5(self):
+        res = app.next5(1.5).response.pop()
+        res = float(res)
+        self.assertEqual(res, 6.5)
 
 if __name__ == "__main__":
     with flask_instance.app_context():
